b1852771f8761d35ecd641b0874b8aebefd5d578
Steve Hoeksema <steve@kotiri.com>
2021-11-02 17:07:13 +1300

    Update sth-console.rb

--- Formula/sth-console.rb
+++ Formula/sth-console.rb
@@ -7,11 +7,11 @@ class SthConsole < Formula
   version "1.10"
   bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
+  if OS.mac? #&& Hardware::CPU.intel?
     url "https://sth-console.phreak.nz/1.10/osx/console"
     sha256 "9a58f5a3078bcfb6d61644c91b6ebd5be0e6249acc0977d5bc8b9dec39aa2cf2"
   end
-  if OS.linux? && Hardware::CPU.intel?
+  if OS.linux? #&& Hardware::CPU.intel?
     url "https://sth-console.phreak.nz/1.10/linux/console"
     sha256 "0cb4ba386a7e2689e407092c9c43986560eaa725fae94d28a13d9b05d55de7fd"
   end
