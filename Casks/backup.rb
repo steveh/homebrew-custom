6e90336d551a817aa5ffefb0c06c8a74bb3a0531
goreleaserbot <goreleaser@kotiri.com>
2026-07-13 05:47:12 +1200

    Brew cask update for backup version 0.4.9

--- Casks/backup.rb
+++ Casks/backup.rb
@@ -61,11 +61,11 @@ cask "backup" do
     end
   end
 
-  version "0.4.8"
+  version "0.4.9"
 
   on_macos do
     on_intel do
-      sha256 "7f04a56ad00a5a2ebb4d0af98d5997153d499d8e5f81c498714c0370273e2fdd"
+      sha256 "eb9dab1fc30d9abf0c2f31909c85d57bee100cb142580614773f791c2262c8b0"
       url "#{GitHubHelper.release_asset_url("#{version}", "backup_#{version}_darwin_x86_64.tar.gz")}",
         header: [
           "Accept: application/octet-stream",
@@ -74,7 +74,7 @@ cask "backup" do
         ]
     end
     on_arm do
-      sha256 "462766cbcddfd1b21fe3d350bcf682ceddad1f0f1ee216fb82b13c3a66a84ea0"
+      sha256 "9ab0824dd7e5b3ba4bb33880b7d3e5c25d4cd981b9d762ca2090c37c7570544b"
       url "#{GitHubHelper.release_asset_url("#{version}", "backup_#{version}_darwin_arm64.tar.gz")}",
         header: [
           "Accept: application/octet-stream",
@@ -86,7 +86,7 @@ cask "backup" do
 
   on_linux do
     on_intel do
-      sha256 "aee04a4427dd70d584f1c9488d2324cd370497df7d501ea40c352f8f536fd24d"
+      sha256 "8ad3108a74171ef160ded544b3eb7176c27b523aad5123eaa6eaf883869e7182"
       url "#{GitHubHelper.release_asset_url("#{version}", "backup_#{version}_linux_x86_64.tar.gz")}",
         header: [
           "Accept: application/octet-stream",
@@ -95,7 +95,7 @@ cask "backup" do
         ]
     end
     on_arm do
-      sha256 "14bd0693134fd08f5d10602e84c09bda871849919b3b672382e892c9c9271bb4"
+      sha256 "f8013aaab4f2193ef074919840aa21af5f646b40d043a119427ca645a1ead01d"
       url "#{GitHubHelper.release_asset_url("#{version}", "backup_#{version}_linux_aarch64.tar.gz")}",
         header: [
           "Accept: application/octet-stream",
